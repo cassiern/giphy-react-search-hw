bccdc589965ce1f279bf3553dc68adc36b2e2013
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-08-27 17:29:45 -0600

    no need to search for caTT anymore, suckaaaaaaa

--- src/App.js
+++ src/App.js
@@ -8,49 +8,58 @@ class App extends Component {
     super();
     this.state = {
       gifs: [],
-      value: ''
+      search: ''
     }
     this.handleChange = this.handleChange.bind(this);
   }
 
 getGifs = async(e) => {
     try{
-      const gifs = await fetch(`http://api.giphy.com/v1/gifs/search?q=${this.state.value}&api_key=ZGkoSXdxLnWORsrAGNxMzwn7vSsmmSi6&limit=20`);
+      const gifs = await fetch(`http://api.giphy.com/v1/gifs/search?q=${this.state.search}&api_key=ZGkoSXdxLnWORsrAGNxMzwn7vSsmmSi6&limit=20`);
       const gifsJson = await gifs.json();
       console.log(gifsJson, 'gifsJson')
-      return gifsJson;
+      //return gifsJson;
+
+      this.setState(
+      {
+        gifs: await gifsJson.data
+      });
     } catch(err) {
       console.log('getGifs err: ', err);
       return err;
     }
-  }
-  getData(){
-  this.getGifs()
-    .then((giphyData) => {
-      console.log('data: ', giphyData);
-      this.setState({
-        gifs: giphyData.data
-      })
-    })
 }
+
+
+// getData(){
+//   this.getGifs()
+//     .then((giphyData) => {
+//       console.log('data: ', giphyData);
+//       this.setState({
+//         gifs: giphyData.data
+//       })
+//     })
+// }
 
 handleChange = (e) =>{
   this.setState({
-    value: e.target.value
+    [e.target.name] : e.target.value
   })
-  this.getData();
+  console.log(e.target.value)
+  //this.getData();  
 }
 
 
 render() {
   console.log(this.state.gifs, 'this.state.gifs in the render');
-  console.log(this.state.value, 'this.state.value')
+  console.log(this.state.search, 'this.state.search')
 
 
         return (
             <div>
             <div className="search">
-                <input type="text" name="search" placeholder="Search..." onChange={this.handleChange} />  
+                <input type="text" name="search" placeholder="Search..." onChange={this.handleChange} />
+                <input type="submit" value="submit" onClick={this.getGifs} /> 
             </div>
                 <GifList gifs={this.state.gifs} />
             </div>
